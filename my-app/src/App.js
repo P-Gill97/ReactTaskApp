ee2eeda30e81389b5690d2563dc05330118596de
P-Gill97 <pgill@csus.edu>
2019-02-11 22:04:37 -0800

    added a component and made a css file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        noteText: '',
+      }
+
+    }
+    updateNoteText(noteText){
+      this.setState({noteText: noteText.target.value})
+    }
+
+    handleKeyPress = =>{
+      if(event.key === 'Enter'){
+          
+
+      }
+
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className = "header">Cross platform Todo Application</div>
+        <div className = "btn">+</div>
+        <input type = "text"
+          ref= {{(input) => {this.textInput = input}}}
+          className = "textInput"
+          value = {this.state.noteText}
+          onChangeText = {noteText => this.updateNoteText(noteText)}
+          onKeyPress = {this.handleKeyPress.bind(this)}
+          />
       </div>
     );
   }
