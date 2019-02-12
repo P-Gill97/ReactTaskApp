b548f869a0ff6555f4bb4122206b425b6f234870
P-Gill97 <pgill@csus.edu>
2019-02-12 00:18:27 -0800

    almost finished with part 1. Will debug and start working on firebase integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,27 +23,37 @@ class App extends Component {
       }
 
     }
+    addNode(){
+      if(this.state.noteText === ''){return}
+      let notesArr = this.state.notes;
+      // resetting state of note to empty
+      this.setState({ noteText: ''})
+      //refocus input field
+      this.textInput.focus();
+    }
     deleteNote(index){
       let notesArr = this.state.notes;
       notesArr.splice(index, 1);
-      this.setState({notes = notesArr})
+      this.setState({notes : notesArr});
+
     }
 
 
   render() {
-    let notes = this.state.map((val, key)) => {
+    let notes = this.state.notes.map((val,key) => {
       return <Note key={key} text = {val}
                       deleteMethod={() => this.deleteNote(key) } />
-    }
+                  })
     return (
       <div className="container">
         <div className = "header">Cross platform Todo Application</div>
+        {notes}
         <div className = "btn" onClick = {this.addNode.bind(this)}>+</div>
         <input type = "text"
           ref= {((input) => {this.textInput = input})}
           className = "textInput"
           value = {this.state.noteText}
-          onChangeText = {noteText => this.updateNoteText(noteText)}
+          onChange = {noteText => this.updateNoteText(noteText)}
           onKeyPress = {this.handleKeyPress.bind(this)}
           />
       </div>
--- my-app/src/components/Note.js
+++ my-app/src/components/Note.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Note extends Component {
 
   render() {
-    return{
+    return(
       <div className = "note" onClick ={ this.props.deleteMethod}>
         {this.props.text}
 
       </div>
-    };
-  }
+    );
+  
 }
-
+}
 export default Note;
